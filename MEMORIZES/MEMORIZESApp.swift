9aec7124bd5d79fece9d3fdf36c306af95fc3037
KUNAL <IKUNALSINGHH@GMAIL.COM>
2024-11-04 19:36:30 +0530

    Initial Commit

---
+++ MEMORIZES/MEMORIZESApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEMORIZESApp.swift
+//  MEMORIZES
+//
+//  Created by Kunal Singh on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct MEMORIZESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MEMORIZESTests/MEMORIZESTests.swift
@@ -0,0 +1,16 @@
+//
+//  MEMORIZESTests.swift
+//  MEMORIZESTests
+//
+//  Created by Kunal Singh on 2024-11-04.
+//
+
+import Testing
+
+struct MEMORIZESTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
